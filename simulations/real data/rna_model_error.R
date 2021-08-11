0795df70eceb7b8422e86034da4d6af667aa8448
White <whitealj@iu.edu>
2021-08-11 16:45:39 -0400

    fix roles in description

--- simulations/real data/rna_model_error.R
+++ simulations/real data/rna_model_error.R
@@ -2,6 +2,7 @@ paths <- .libPaths()
 paths <- c("/geode2/home/u100/whitealj/Carbonate/r_4_0_4_library/",paths)
 .libPaths(paths)
 library(hthmixture)
+options(error=traceback)
 load("/geode2/home/u100/whitealj/Carbonate/scripts/COAD_tumor_logFPKM_Methylation.RData")
 
 x <- COAD_Methylation450 %>% 
@@ -13,7 +14,6 @@ y <- COAD_logFPKM %>%
   t() %>% 
   dplyr::as_tibble()
 
-safe_hthmix <- purrr::safely(hthmix_cv)
-model <- safe_hthmix(x, y, k_min = 2, k_max = 6, r_min = 1, r_max = 2, chains = 1, penal_search = 1:100/100)
+model <- hthmix_cv(x, y, k_min = 2, k_max = 6, r_min = 1, r_max = 2, chains = 1, penal_search = 1:100/100)
 
 model %>% saveRDS("rna_results_20210802.rds")
